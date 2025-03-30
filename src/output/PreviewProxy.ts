af761d3428fcc6448442c2ab771611a70da30bf3
again13 <againhoffman@yahoo.com>
2025-03-30 21:10:34 +0000

    Update auth - removing deprecated function

--- src/output/PreviewProxy.ts
+++ src/output/PreviewProxy.ts
@@ -1,7 +1,7 @@
 // ReplProxy and srcdoc implementation from Svelte REPL
 // MIT License https://github.com/sveltejs/svelte-repl/blob/master/LICENSE
 
-let uid = 1
+var uid = 1
 
 export class PreviewProxy {
   iframe: HTMLIFrameElement
@@ -37,15 +37,15 @@ export class PreviewProxy {
   }
 
   handle_command_message(cmd_data: any) {
-    let action = cmd_data.action
-    let id = cmd_data.cmd_id
-    let handler = this.pending_cmds.get(id)
+    var action = cmd_data.action
+    var id = cmd_data.cmd_id
+    var handler = this.pending_cmds.get(id)
 
     if (handler) {
       this.pending_cmds.delete(id)
       if (action === 'cmd_error') {
-        let { message, stack } = cmd_data
-        let e = new Error(message)
+        var { message, stack } = cmd_data
+        var e = new Error(message)
         e.stack = stack
         handler.reject(e)
       }
